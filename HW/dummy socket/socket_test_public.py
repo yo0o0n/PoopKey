66da6b1baee46926fc8116f8c5365cc067d20470
이진수 <leejinsoo0412@gmail.com>
2024-01-31 13:05:41 +0900

    Feat: 포트 수정 및 종료

--- HW/dummy socket/socket_test_public.py
+++ HW/dummy socket/socket_test_public.py
@@ -1,6 +1,6 @@
 import socket
 public_server_ip = "70.12.108.71"
-public_server_port = 8001
+public_server_port = 10200
 public_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 public_socket.connect((public_server_ip, public_server_port))
 
@@ -13,3 +13,4 @@ print("wait")
 rec = public_socket.recv(1024).decode().strip()
 print(rec)
 
+public_socket.close()

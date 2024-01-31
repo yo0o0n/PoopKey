aa834939e34981f8782ccc1ba06eeaa5d80adedd
이진수 <leejinsoo0412@gmail.com>
2024-01-31 12:45:08 +0900

    Fix : 퍼블릭 ip 소켓 테스트 오류 수정

--- HW/dummy socket/socket_test_public.py
+++ HW/dummy socket/socket_test_public.py
@@ -1,10 +1,15 @@
 import socket
 public_server_ip = "70.12.108.71"
-public_server_port = 1104
+public_server_port = 8001
 public_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 public_socket.connect((public_server_ip, public_server_port))
 
 # RESTROOM_ID = 1
 # congestion = 2
 # backend_socket.sendall(("congestion,%d,%d,"%(congestion, RESTROOM_ID)).encode())
-import pdb; pdb.set_trace()
+print("connected")
+public_socket.sendall("hihi".encode())
+print("wait")
+rec = public_socket.recv(1024).decode().strip()
+print(rec)
+

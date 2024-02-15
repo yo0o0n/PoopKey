936f870c926f4092c8f9d9065fad0f72b761771e
Your Name <you@example.com>
2024-02-15 13:36:27 +0900

    산출물 DB 덤프파일 업로드

---
+++ exec/DBdump/poopkey_stall.sql
@@ -0,0 +1,60 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: poopkey
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `stall`
+--
+
+DROP TABLE IF EXISTS `stall`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stall` (
+  `stall_id` bigint NOT NULL AUTO_INCREMENT,
+  `restroom_id` int NOT NULL,
+  `status` int NOT NULL DEFAULT '0' COMMENT '사용가능:0, 사용중:1, 고장:2, 점검:3, 고장의심:4',
+  `last_used_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '화장실을 사용했을 때 갱신',
+  `closed` int NOT NULL DEFAULT '0' COMMENT '0:열림, 1:잠김',
+  `used_number_after_cleaning` int NOT NULL DEFAULT '0' COMMENT '청소 후 0 으로 reset',
+  `maintaining` int NOT NULL DEFAULT '0' COMMENT '0:점검x, 1:점검중',
+  `row` int NOT NULL DEFAULT '1' COMMENT '인덱스 번호 1부터 시작',
+  `col` int NOT NULL DEFAULT '1' COMMENT '인덱스 번호 1부터 시작',
+  `content` int NOT NULL DEFAULT '0' COMMENT '0:빈칸, 1:화장실, 2:입구',
+  `toilet_status` int NOT NULL DEFAULT '0' COMMENT '변기 막힘 여부는 화장실 칸 table의 고장 column으로 반영된다. 0:변기막힘 x, 1:변기막힘',
+  PRIMARY KEY (`stall_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stall`
+--
+
+LOCK TABLES `stall` WRITE;
+/*!40000 ALTER TABLE `stall` DISABLE KEYS */;
+INSERT INTO `stall` VALUES (1,1,0,'2024-02-15 04:29:18',0,0,0,1,1,1,0),(2,1,1,'2024-02-15 04:29:18',1,1,0,1,2,1,0),(3,1,0,'2024-02-15 04:29:18',0,2,0,1,3,1,0),(4,1,0,'2024-02-15 04:29:18',0,0,0,2,1,0,0),(5,1,0,'2024-02-15 04:29:18',0,0,0,2,2,0,0),(6,1,0,'2024-02-15 04:29:18',0,0,0,2,3,0,0),(7,1,0,'2024-02-15 04:29:18',0,0,0,3,1,0,0),(8,1,0,'2024-02-15 04:29:18',0,0,0,3,2,0,0),(9,1,0,'2024-02-15 04:29:18',0,0,0,3,3,2,0),(10,2,0,'2024-02-15 04:29:18',0,0,0,1,1,1,0),(11,2,2,'2024-02-15 04:29:18',0,3,0,1,2,1,1),(12,2,1,'2024-02-15 04:29:18',1,4,0,1,3,1,0),(13,2,0,'2024-02-15 04:29:18',0,0,0,2,1,0,0),(14,2,0,'2024-02-15 04:29:18',0,0,0,2,2,0,0),(15,2,0,'2024-02-15 04:29:18',0,0,0,2,3,0,0),(16,2,0,'2024-02-15 04:29:18',0,0,0,3,1,0,0),(17,2,0,'2024-02-15 04:29:18',0,0,0,3,2,0,0),(18,2,0,'2024-02-15 04:29:18',0,0,0,3,3,2,0),(19,3,0,'2024-02-15 04:29:18',0,0,0,1,1,1,0),(20,3,3,'2024-02-15 04:29:18',0,0,1,1,2,1,0),(21,3,2,'2024-02-15 04:29:18',0,2,0,1,3,1,0),(22,3,0,'2024-02-15 04:29:18',0,0,0,2,1,0,0),(23,3,0,'2024-02-15 04:29:18',0,0,0,2,2,0,0),(24,3,0,'2024-02-15 04:29:18',0,0,0,2,3,0,0),(25,3,0,'2024-02-15 04:29:18',0,0,0,3,1,0,0),(26,3,0,'2024-02-15 04:29:18',0,0,0,3,2,0,0),(27,3,0,'2024-02-15 04:29:18',0,0,0,3,3,2,0),(28,4,1,'2024-02-15 04:29:18',1,2,0,1,1,1,0),(29,4,1,'2024-02-15 04:29:18',1,3,0,1,2,1,0),(30,4,1,'2024-02-15 04:29:18',1,5,0,1,3,1,0),(31,4,0,'2024-02-15 04:29:18',0,0,0,2,1,0,0),(32,4,0,'2024-02-15 04:29:18',0,0,0,2,2,0,0),(33,4,0,'2024-02-15 04:29:18',0,0,0,2,3,0,0),(34,4,0,'2024-02-15 04:29:18',0,0,0,3,1,0,0),(35,4,0,'2024-02-15 04:29:18',0,0,0,3,2,0,0),(36,4,0,'2024-02-15 04:29:18',0,0,0,3,3,2,0);
+/*!40000 ALTER TABLE `stall` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-02-15 13:31:08

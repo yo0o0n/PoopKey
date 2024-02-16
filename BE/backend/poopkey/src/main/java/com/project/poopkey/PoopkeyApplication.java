3ad36ea228ce9b97c03025c32eda1b5c60cd2411
서해광 <shkhk@naver.com>
2024-02-16 11:07:20 +0900

    Update BE/backend/poopkey/src/main/java/com/project/poopkey/PoopkeyApplication.java

--- BE/backend/poopkey/src/main/java/com/project/poopkey/PoopkeyApplication.java
+++ BE/backend/poopkey/src/main/java/com/project/poopkey/PoopkeyApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-// 아직 시큐리티 인증 부분을 구현하지 않았다.
 @ComponentScan(basePackages = {"com.project.poopkey.netty","com.project.poopkey.clientsocket", "com.project.poopkey.application.main.security"})
 //@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // 스프링 부트 임시 해제
 @SpringBootApplication
